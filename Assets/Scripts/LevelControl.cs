bcd97cf38ea6696c91a407a02a65f6410988e996
AlexGama11 <52133865+AlexGama11@users.noreply.github.com>
2019-12-03 12:28:51 +0000

    Add files via upload

---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,24 @@
+﻿// Libraries unity is going to use
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// Name of the object
+public class LevelControl : MonoBehaviour
+{
+	// a variable which allows us to specify which scene we want to load.
+	
+	public string LevelOne;
+	public string MainMenu;
+	
+	public void BackToMain()
+	{
+	    // This line will trigger scene manager to load the main menu.
+		        SceneManager.LoadScene(MainMenu);
+	}
+	// This line will make the player exit the application.
+	
+	public void QuitGame()
+	{
+	 	Application.Quit();
+	}
+}
+	
